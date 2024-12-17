0d905e63e4a3c2e571a2ff0c5ac3b7ab0195a6df
Imre Tabur <imre.tabur@mail.ee>
2024-12-17 17:09:21 +0200

    Some more notes. Cleanup from obious things.

--- apache-low-io-core/src/main/java/org/apache/low/io/core/Record.java
+++ apache-low-io-core/src/main/java/org/apache/low/io/core/Record.java
@@ -1,9 +1,9 @@
-package org.apache.low.io.core.models;
+package org.apache.low.io.core;
 
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 
 @Builder(toBuilder = true)
 @RequiredArgsConstructor
-public class EventData {
+public class Record {
 }
