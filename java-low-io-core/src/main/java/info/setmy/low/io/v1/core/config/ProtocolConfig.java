15a5adf17ddc665dd9781c6ba0185fed69cfb8da
Imre Tabur <imre.tabur@mail.ee>
2025-05-30 21:22:10 +0300

    Some changes

---
+++ java-low-io-core/src/main/java/info/setmy/low/io/v1/core/config/ProtocolConfig.java
@@ -0,0 +1,10 @@
+package info.setmy.low.io.v1.core.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public final class ProtocolConfig {
+
+}
